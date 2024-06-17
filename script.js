310e09dc020f03b1c658d3ef4dcd79c7bc4c657d
denmarkcruz09 <denmarkcruz09@gmail.com>
2024-06-17 16:38:23 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Optional JavaScript for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop - 70,
+            behavior: 'smooth'
+        });
+    });
+});
